7010b3812f6e4da44cede4fb9882652eae67a89e
Anton Kuchmii <a.kuchmiy19@gmail.com>
2022-02-06 18:16:35 +0200

    fix tailwind purges dark class

--- esbuild.js
+++ esbuild.js
@@ -5,19 +5,27 @@ const autoprefixer = require('autoprefixer')
 const tailwindcss = require('@tailwindcss/postcss7-compat')
 const postcssImport = require('postcss-import')
 
-esbuild
-	.build({
-		entryPoints: ['src/index.ts'],
-		bundle: true,
-		minify: true,
-		format: 'esm',
-		sourcemap: true,
-		outdir: 'dist',
-		plugins: [
-			esbuildCssModulesPlugin({}),
-			postCssPlugin({
-				plugins: [postcssImport, tailwindcss, autoprefixer],
-			}),
-		],
-	})
-	.catch((error) => console.log(error))
+;(async () => {
+	try {
+		const result = await esbuild.build({
+			entryPoints: ['src/index.ts'],
+			bundle: true,
+			minify: true,
+			format: 'esm',
+			sourcemap: true,
+			outdir: 'dist',
+			metafile: true,
+			plugins: [
+				esbuildCssModulesPlugin({}),
+				postCssPlugin({
+					plugins: [postcssImport, tailwindcss, autoprefixer],
+				}),
+			],
+		})
+
+		let text = await esbuild.analyzeMetafile(result.metafile)
+		console.log(text)
+	} catch (error) {
+		console.log(error)
+	}
+})()
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
 	purge: {
 		enabled: true,
 		content: ['./src/**/*.tsx', './src/**/*.css', './src/**/*.ts'],
+		options: {
+			safelist: ['dark'],
+		},
 	},
 	darkMode: 'class',
 }
